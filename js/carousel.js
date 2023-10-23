a987e28f28452abf523c9286028015f6b34b729a
Gerardo González García <spartano1994@hotmail.com>
2023-10-22 19:54:22 -0600

    reparó el rendimiento de Youtube

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+const imagesCollection = {
+}
+
+export function createCarousel() {
+    const carouselContainer = document.querySelector( "[data-carousel]" )
+
+}
+
+function createSlide(){
+    const slide = document.createElement( "div" );
+}
