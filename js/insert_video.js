01813074cc1c046466bd5d72e42c37c7cde946fe
Gerardo González García <spartano1994@hotmail.com>
2023-10-22 21:41:57 -0600

    se quitó la librería externa de YT y se realizó la inserción a mano

---
+++ js/insert_video.js
@@ -0,0 +1,15 @@
+const btnIndex = document.querySelector( "[data-btnindex]" );
+
+console.log( btnIndex ) 
+
+function insertarVideo(){
+    const cartel = document.querySelector( "[data-cartel]" );
+
+    cartel.style.backgroundImage = "none";
+    cartel.innerHTML = "";
+    cartel.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/LEwXRUB6LP4?si=p7UXIc-fqccYaewF" title="YouTube video player" frameborder="0" allow="accelerometer"; autoplay="1"; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>' ;
+
+    
+}
+
+btnIndex.addEventListener( "click" , insertarVideo );
