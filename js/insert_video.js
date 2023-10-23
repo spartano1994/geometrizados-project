5776b46ade1eced002d837e97a283791fe5ec6ba
Gerardo González García <spartano1994@hotmail.com>
2023-10-23 14:57:15 -0600

    Se configuró la parte de comentarios y se inició la página who

--- js/insert_video.js
+++ js/insert_video.js
@@ -5,7 +5,7 @@ function insertarVideo(){
 
     cartel.style.backgroundImage = "none";
     cartel.innerHTML = "";
-    cartel.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/LEwXRUB6LP4?si=p7UXIc-fqccYaewF" title="YouTube video player" frameborder="0" allow="accelerometer"; autoplay="1"; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>' ;
+    cartel.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/LEwXRUB6LP4?si=p7UXIc-fqccYaewF" title="YouTube video player" frameborder="0" allow="accelerometer" ; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>' ;
 
     
 }
